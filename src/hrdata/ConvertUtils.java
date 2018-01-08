a9eb0c13106b1cc4f4939324594261cd25e5026d
onekriach <onekriach@C-00695.lv.dyninno.lan>
2018-01-08 18:02:11 +0200

    Add comments to file

--- src/hrdata/ConvertUtils.java
+++ src/hrdata/ConvertUtils.java
@@ -12,8 +12,11 @@ import org.apache.commons.lang.StringUtils;
  * @author onekriach
  */
 //http://www.unitconversion.org/numbers/base-10-to-base-36-conversion.html
+//You can find useful link how to convert to base36 in different languages https://en.wikipedia.org/wiki/Base36
+
 public class ConvertUtils {
 
+    private static final String personId = "99999999999";
     private static final String CODE_BASE_36 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final Character ZERO = '0';
 
@@ -43,61 +46,46 @@ public class ConvertUtils {
      * @param numToConvert The number to convert
      * @param length The length of the final result
      * @return The number passed in parameter converted to base 36 on x
-     * characters (x corresponding to length, the 2nd parameter)
+     * characters (x corresponding to length, the 2nd parameter). Return string has fixed length.
      */
     public static String toBase36(Long numToConvert, int length) {
         return StringUtils.leftPad(toBase36(numToConvert), length, ZERO);
     }
 
-//    public String getHrIdNumber(String personId, String salt) {
-//        String hrIdNumber = "";
-//        String idSup = personId.replaceAll("[^\\d]", "");
-//        //System.out.println("idSup="+idSup);
-//
-//        String empSup = salt.replaceAll("[^\\d]", "");
-//        //System.out.println("empSup="+empSup);
-//
-//        BigInteger idSupB = new BigInteger(idSup);
-//        BigInteger empSupB = new BigInteger(empSup);
-//        BigInteger sum;
-//        sum = idSupB.add(empSupB);
-//        //System.out.println("sum="+sum);
-//
-//        long longNumber = Long.parseLong(sum.toString());
-//        hrIdNumber = this.toBase36(longNumber, 12);
-//        //System.out.println("Base36="+hrIdNumber);
-//
-//        return hrIdNumber;
-//    }
-
     public String getHrIdNumberRev(String personId, String salt) {
         String hrIdNumber = "";
+        //replace all none digital charachters
         String idSup = personId.replaceAll("[^\\d]", "");
-        //System.out.println("idSup="+idSup);
-
         String empSup = salt.replaceAll("[^\\d]", "");
-        //System.out.println("empSup="+empSup);
-
+       // Get encoded string in long
         long longNumber = Long.parseLong(this.encodeString(idSup, empSup));
-        //System.out.println("longNumber="+longNumber);
+        
+        // Convert long string to Base 36. Employee should have fixed length in 12 symbols
         hrIdNumber = this.toBase36(longNumber, 12);
-        //System.out.println("Base36="+hrIdNumber);
-
         return hrIdNumber;
     }
-
+    /**
+     * Get encoded string by encode string with a salt
+     *
+     * @param personId The string to encode
+     * @param salt The salt of encoded string
+     * @return The encoded string
+     */
     public String encodeString(String personId, String salt) {
         String output = null;
+        //Reverse encoded string
         String reversePersonId = new StringBuffer(personId).reverse().toString();
-        //System.out.println("reversePersonId="+reversePersonId);
-        String saltSub=null;
-        if(salt.length() > 6){
-        int cut=salt.length()-6;
-        saltSub = salt.substring(cut);
+
+        //Salt should have some length (if salt length > 6 then use the last 6 digits)
+        String saltSub = null;
+        if (salt.length() > 6) {
+            int cut = salt.length() - 6;
+            saltSub = salt.substring(cut);
         } else {
-        saltSub=salt;
+            saltSub = salt;
         }
-        //System.out.println("saltSub="+saltSub);
+        
+        //Insert salt symbol by symbol after every 2 reversePersonId characters 
         char[] ret_arr = new char[reversePersonId.length() + saltSub.length()];
         char[] id_arr = reversePersonId.toCharArray();
         char[] s_arr = saltSub.toCharArray();
@@ -112,18 +100,18 @@ public class ConvertUtils {
             ret_arr[k] = id_arr[i];
             k = k + 1;
         }
-        output=new String(ret_arr);      
+        output = new String(ret_arr);
 
         return output;
     }
 
     public static void main(String[] args) {
-        String personId = "99999999999";
-        String salt = "123456789";//"25" "960"
+        //example PID 
+        String pid = "2369";
         ConvertUtils c = new ConvertUtils();
-
-        //System.out.println("getHrIdNumber=" + c.getHrIdNumberRev(personId, salt));
-        //System.out.println("getHrIdNumber=" + c.encodeString(personId, salt));
+        String employeeId = c.getHrIdNumberRev(personId, pid); //For HRM personId always is 99999999999
+        System.out.println("Result should be equal: 002Y4MBN3X1B = " + employeeId);
+        System.out.println("Result is correct: " + employeeId.equals("002Y4MBN3X1B"));
     }
 
 }
